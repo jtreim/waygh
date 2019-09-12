11f9df47350065dc022b74634c5538388a8b2366
jtreim <email.jreim@gmail.com>
2019-09-11 20:48:03 -0600

    Initial commit

---
+++ app/src/main/java/com/example/waygh/StartActivity.java
@@ -0,0 +1,26 @@
+package com.example.waygh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        final Button startBtn = (Button) findViewById(R.id.start_btn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, MapsActivity.class);
+                StartActivity.this.startActivity(intent);
+            }
+        });
+    }
+}
